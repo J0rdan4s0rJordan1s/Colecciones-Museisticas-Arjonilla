2560dedd30ababaeee65110c36d4c41b9fab1548
Aaron <buny01234@outlook.com>
2025-02-19 13:44:47 +0100

    Creado colecciones

---
+++ lib/colecciones.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class Colecciones extends StatelessWidget {
+  final List<Map<String, String>> colecc = [
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    },
+    {
+      "title": "Introducción",
+      "image": "assets/images/colecciones/colecciones-arjonilla-1.jpg"
+    }
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AUDIOGUÍAS COLECCIONES"),
+        backgroundColor: Colors.black,
+      ),
+      );
+  }
+}
