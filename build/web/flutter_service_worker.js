54b62bf581d6684324e35c16bef6ea605a4dcf21
Aaron <buny01234@outlook.com>
2025-03-07 12:14:28 +0100

    Creado Web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "99cf72bdd3e463a8be9d4be454d70cbc",
+const RESOURCES = {"flutter_bootstrap.js": "1a2770eaa398792cad8094dc349425d5",
 "version.json": "55067a20a55fb044c02a90028a674666",
 "index.html": "3b72291f960840be806820b79cba23b8",
 "/": "3b72291f960840be806820b79cba23b8",
-"main.dart.js": "11401144ce7354919f43ea5443d072d5",
+"main.dart.js": "9d08be9092f88ff50a5d45824ad04461",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
